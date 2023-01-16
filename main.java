fbaa3a05d86a173326a18fadd15688e23f99b5ed
Smachy <pro.diegocdossantos@gmail.com>
2023-01-16 16:27:33 +0100

    test

--- main.java
+++ main.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
-
+import java.awt.Color;
+import javax.swing.JColorChooser; // importer de java 
 import Classes.*;
 
 public class main {
     public static void main(String[] args) {
+
+        Player joueur = new Player("bob");
+
+        Color couleurChoisie = JColorChooser.showDialog(null, "couleur pour les jetons ", joueur.getCouleur());  // choisir la couleur 
+    
+        joueur.setCouleur(couleurChoisie);
+        System.out.println(joueur.getCouleur());
         
         Grid grid = new Grid();
         grid.createGrid();
@@ -24,4 +32,6 @@ public class main {
 
 
     }
+
+    
 }
