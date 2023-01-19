fc18e242f14728e4ff83af7f3bd7181853f923d3
Smachy <pro.diegocdossantos@gmail.com>
2023-01-19 20:51:11 +0100

    docstring player comparator

--- Classes/PlayerComparator.java
+++ Classes/PlayerComparator.java
@@ -4,6 +4,12 @@ import java.util.Comparator;
 public class PlayerComparator implements Comparator<Player> {
     @Override
     public int compare(Player o1, Player o2) {
+        /*
+         * Override compare function to sort arraylist by scores
+         * @param o1(Player): player 1
+         * @param o2(Player): player 2
+         * @return (int): score player2 - score player1
+         */
         return o2.getscore() - o1.getscore();
     }
 }
